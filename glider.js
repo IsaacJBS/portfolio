f9f62039f2139fdbd7cf316a2d7c7952a42b0c5b
Isaac <isaac.jordaobs@gmail.com>
2022-04-05 19:37:25 -0300

    feat: carrossel quase pronto

---
+++ glider.js
@@ -0,0 +1,11 @@
+const carousel = document.querySelector('.glider_slide')
+
+new Glider(carousel, {
+  slidesToScroll: 1,
+  slidesToShow: 2.5,
+  draggable: true,
+  arrows: {
+    prev: '.carousel-prev',
+    next: '.carousel-next'
+  }
+})
