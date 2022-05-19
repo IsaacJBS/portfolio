b0e6caf37dbc1afa079734e7ed6a8c4cc4175782
Isaac <isaac.jordaobs@gmail.com>
2022-05-19 15:14:43 -0300

    fix: responsividade 1024px

--- glider.js
+++ glider.js
@@ -1,11 +1,22 @@
 const carousel = document.querySelector(".glider_slide");
+const numberOfSlides = 9;
 
 new Glider(carousel, {
-  slidesToScroll: 9,
+  slidesToScroll: numberOfSlides,
   slidesToShow: 1.5,
   draggable: true,
-  arrows: {
-    prev: ".carousel-prev",
-    next: ".carousel-next",
-  },
+  // arrows: {
+  //   prev: ".carousel-prev",
+  //   next: ".carousel-next",
+  // },
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 0.5,
+        slidesToScroll: numberOfSlides,
+        itemWidth: 200,
+      },
+    },
+  ],
 });
